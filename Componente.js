09c1651d15037a14c80303bd79bdc9660aac1fd8
Sandra Quero <sanquesan@gmail.com>
2021-10-12 18:49:22 +0200

    Añadimos componente crear elementos

--- Componente.js
+++ Componente.js
@@ -1,35 +1,21 @@
-import Asesor from "./Asesor.js";
-import Escudero from "./Escudero.js";
-import Luchador from "./Luchador.js";
-import Personaje from "./Personaje.js";
-import Rey from "./Rey.js";
-import Componente from "./Componente.js";
+class Componente {
+  elemento;
+  elementoPadre;
+  className;
+  etiquetaHtml;
+  constructor(elementoPadre, className, etiquetaHtml) {
+    this.etiquetaHtml = etiquetaHtml;
+    this.elemento = document.createElement(etiquetaHtml);
+    this.elementoPadre = elementoPadre;
+    this.elemento.className = className;
 
-const joffrey = new Rey("Joffrey", Personaje.familia.arryn, 17, 2);
-const jaime = new Luchador("Jaime", Personaje.familia.stark, 30, "AK-47", 7);
-const daenerys = new Luchador("Daenerys", Personaje.familia.stark, 20, "AK", 7);
-const tyrion = new Asesor("Tyirion", Personaje.familia.stark, 28, daenerys);
-const bronn = new Escudero("Bronn", Personaje.familia.stark, 28, jaime, 7);
+    this.elementoPadre.appendChild(this.elemento);
+  }
+}
 
-const personajes = [joffrey, jaime, daenerys, tyrion, bronn];
-console.log(personajes);
-
-const comunicados = personajes
-  .filter((personaje) => personaje instanceof Luchador)
-  .map((personaje) => personaje.comunicar());
-
-console.log(joffrey.serie);
-comunicados.forEach((mensaje) => console.log(mensaje));
-console.log(`Jaime vivo ? ${jaime.vivo}`);
-console.log(`Tyirion vivo ? ${tyrion.vivo}`);
-jaime.morir();
-tyrion.morir();
-console.log(`Jaime vivo ? ${jaime.vivo}`);
-console.log(`Tyirion vivo ? ${tyrion.vivo}`);
-
-console.log(personajes);
-
-/* class Componente {
+export default Componente;
+/* 
+class Componente {
   constructor(arrayPersonajes) {
     if (Array.isArray(arrayPersonajes)) {
       const contenedorLista = document.getElementsByClassName(
@@ -116,13 +102,8 @@ console.log(personajes);
         document.querySelectorAll("character__action btn").
         addEventListener("click", (personaje) => {
           console.log("hola");
-        }); 
-      });
+        });
+      }
     }
-  }
-} */
-
-const ul = document.querySelector(".characters-list");
-console.log(ul);
-const tarjeta = new Componente(ul, "character col", "li");
-console.log(tarjeta);
+  } 
+}); */
